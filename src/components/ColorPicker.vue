5e261789603ffbd7ce4b76380723b521491d326f
umer khan <umer.kdev@gmail.com>
2018-09-28 23:53:38 +0300

    adding color picker

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="colorpicker">
+
+    </div>
+</template>
+
+<script>
+
+
+
+    export default {
+        name: 'ColorPicker',
+        props : {
+            color : String
+        }
+
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
